0418f8d42835c0185bfbba25ae6aa58de1c66587
spoerri <spoerri@users.noreply.github.com>
2017-11-13 22:09:52 -0500

    Update Test.java

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -3,5 +3,15 @@ package test;
 public class Test {
 	@org.junit.Test
 	public void foo() {
+		int c = 0;
+		MyEntity myEntity = new MyEntityObservable(new MyEntityImpl(), new Observer<MyEntity, Void> {
+	Void beforeChange(MyEntity t) { return null; }
+	void afterChange(MyEntity t, Void v) { c++; }
+});
+		myEntity.f();
+		myEntity.f();
+		myEntity.f();
+		assertEquals(myEntity.g(), 42);
+		assertEquals(c, 3);
 	}
 }
